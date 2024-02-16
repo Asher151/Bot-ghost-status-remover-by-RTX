68e5124a09b5c1889de4ed4e1575be8710614602
Asher151 <148594142+Asher151@users.noreply.github.com>
2024-02-15 22:23:51 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching over Sync"];
 
 
 let currentIndex = 0;
